35ef4dea7d867b8279c0b04974ba76b895fe619a
AIPC <grillnov@gmail.com>
2018-02-21 19:38:31 -0500

    Added the header

---
+++ ProjectSolid/FEMSolid.h
@@ -0,0 +1,12 @@
+#pragma once
+/*Put every last 3rd party we need in this header*/
+# include <iostream>
+# include <cublas.h>
+# include <cuda.h>
+# include <Eigen\Sparse>
+# include <tetgen.h>
+
+class FEMSolid
+{
+
+};
